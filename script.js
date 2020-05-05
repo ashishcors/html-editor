43a40032e4932c4dca048cae5243fb274bf09945
Ashish Suman <ashish.ranchi1@gmail.com>
2020-05-06 00:57:05 +0530

    added logic to add code

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 editor.document.designMode = "On";
+window.onload = function () {
+  let head  = editor.document.getElementsByTagName('head')[0];
+  let link  = editor.document.createElement('link');
+  link.rel  = 'stylesheet';
+  link.type = 'text/css';
+  link.href = 'prism/prism.css';
+  head.appendChild(link);
+
+  let body  = editor.document.getElementsByTagName('body')[0];
+  let script  = editor.document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'prism/prism.js';
+  body.appendChild(script);
+}
 
 function transform(option, argument) {
   editor.document.execCommand(option, false, argument);
@@ -10,10 +24,13 @@ function onCreateLinkClick(){
 }
 
 function onCodeBlockClick(){
+  let parent = editor.document.getSelection().focusNode.parentElement;
+  if(parent.tagName != 'BODY') return;
   let codeBlock = editor.document.createElement('pre');
   let subCodeBlock = editor.document.createElement('code');
   codeBlock.appendChild(subCodeBlock);
   codeBlock.classList.add('language-css');
+  parent.appendChild(codeBlock);
 }
 
 // window.onload = function () {
