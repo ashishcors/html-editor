4e115bd66d17c14cf3076e202437f48258390db2
Ashish Suman <ashish.ranchi1@gmail.com>
2020-05-07 00:44:50 +0530

    added improvements for fonts

--- script.js
+++ script.js
@@ -12,6 +12,19 @@ window.onload = function () {
   script.type = 'text/javascript';
   script.src = 'prism/prism.js';
   body.appendChild(script);
+
+  body.style.fontFamily = "Segoe UI";
+  
+  const dropdownStyle = new DropDown(document.querySelector("#style-button"));
+  dropdownStyle.init();
+  
+  const dropdownFont = new DropDown(document.querySelector("#font-button"));
+  dropdownFont.init();
+
+  document.querySelector('body').addEventListener('click', (e) =>{
+    if(e.target != dropdownStyle.element) dropdownStyle.hide();
+    if(e.target != dropdownFont.element) dropdownFont.hide();
+  })
 }
 
 function transform(option, argument) {
@@ -33,9 +46,39 @@ function onCodeBlockClick(){
   parent.appendChild(codeBlock);
 }
 
-// window.onload = function () {
-//   let btnCode = document.querySelector('#btn-code');
-//   btnCode.addEventListener('click', function(){
-    
-//   })
-// }
+class DropDown{
+  constructor(element){
+    this.element = element;
+    this.listDiv = document.createElement('div');
+  }
+
+  init(){
+    const content = document.querySelector(`#${this.element.id} .dropdown-content`);
+    const children = document.querySelectorAll(`#${this.element.id} .dropdown-item`);
+    children.forEach((child) =>{
+      const originalOnClick = child.onclick;
+      child.addEventListener('click', () =>{
+        originalOnClick();
+        this.element.childNodes[0].nodeValue = child.innerText;
+      }) 
+    })
+
+    this.element.removeChild(content);
+    this.listDiv.classList.add('dropdown-div');
+    this.listDiv.appendChild(content);
+    this.element.appendChild(this.listDiv);
+
+    this.element.addEventListener('click', (e) =>{
+      if(e.target != this.element && e.target.tagName != "svg" && e.target.tagName != "path") return;
+      this.listDiv.classList.toggle('active');
+    })
+
+    this.listDiv.addEventListener('click', () =>{
+      this.listDiv.classList.remove('active');
+    })
+  }
+
+  hide(){
+    this.listDiv.classList.remove('active');
+  }
+}
