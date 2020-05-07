d79c53f642f6cabaa623f5a2610605340eb3c7b6
Ashish Suman <ashish.ranchi1@gmail.com>
2020-05-08 00:33:16 +0530

    improved code adding logic and documentation

--- script.js
+++ script.js
@@ -1,84 +1,129 @@
+//enable html design mode for editor
 editor.document.designMode = "On";
+
+//initialize everything went the window loads
 window.onload = function () {
-  let head  = editor.document.getElementsByTagName('head')[0];
-  let link  = editor.document.createElement('link');
-  link.rel  = 'stylesheet';
+  //since the editor is an iframe, it doesn't contain any script or css.
+  //following code add prism.css to the editor
+  let head = editor.document.getElementsByTagName('head')[0];
+  let link = editor.document.createElement('link');
+  link.rel = 'stylesheet';
   link.type = 'text/css';
   link.href = 'prism/prism.css';
   head.appendChild(link);
 
-  let body  = editor.document.getElementsByTagName('body')[0];
-  let script  = editor.document.createElement('script');
+  //following code add prism.js to editor
+  let body = editor.document.getElementsByTagName('body')[0];
+  let script = editor.document.createElement('script');
   script.type = 'text/javascript';
   script.src = 'prism/prism.js';
   body.appendChild(script);
 
-  body.style.fontFamily = "Segoe UI";
-  
+  //set the default of the editor to Verdana
+  body.style.fontFamily = "Verdana";
+
+  //initializes the dropdown for text-style button
   const dropdownStyle = new DropDown(document.querySelector("#style-button"));
   dropdownStyle.init();
-  
+
+  //initializes th efromdown for font-family button 
   const dropdownFont = new DropDown(document.querySelector("#font-button"));
   dropdownFont.init();
 
-  document.querySelector('body').addEventListener('click', (e) =>{
-    if(e.target != dropdownStyle.element) dropdownStyle.hide();
-    if(e.target != dropdownFont.element) dropdownFont.hide();
+  //to hide the dropdowns of the use clicks on outside the dropdown
+  //TODO:The dropdown doesn't hides when clicked on editor area.
+  document.querySelector('body').addEventListener('click', (e) => {
+    if (e.target != dropdownStyle.element) dropdownStyle.hide();
+    if (e.target != dropdownFont.element) dropdownFont.hide();
   })
 }
 
+//to execute commands on editor
 function transform(option, argument) {
   editor.document.execCommand(option, false, argument);
 }
 
-function onCreateLinkClick(){
-  var link = window.prompt('Enter link URL'); 
-  transform('createLink',link);
+
+function onCreateLinkClick() {
+  var link = window.prompt('Enter link URL');
+  transform('createLink', link);
 }
 
-function onCodeBlockClick(){
+function onCodeBlockClick() {
   let parent = editor.document.getSelection().focusNode.parentElement;
-  if(parent.tagName != 'BODY') return;
-  let codeBlock = editor.document.createElement('pre');
-  let subCodeBlock = editor.document.createElement('code');
-  codeBlock.appendChild(subCodeBlock);
-  codeBlock.classList.add('language-css');
-  parent.appendChild(codeBlock);
+
+  //if we don't do this the tag gets attached to the main content, not the editor
+  if (parent.tagName != 'BODY') return;
+
+  //get current selection & range
+  let selection = editor.window.getSelection();
+  let range = selection.getRangeAt(0);
+  let selectedText = range.extractContents();
+
+  //code tag needs to be inside pre tag
+  //both tags need to have language class
+  //selected text should be inside code tag
+
+  //create pre tag and code tag and add content to code tag, add css
+  let preTag = editor.document.createElement('pre');
+  let codeTag = editor.document.createElement('code');
+  codeTag.classList.add('language-css');
+  codeTag.appendChild(selectedText);
+  preTag.appendChild(codeTag);
+  preTag.classList.add('language-css');
+  range.insertNode(preTag);
+
+  //TODO: fix issue - when a empty code block is created the code is entered inside pre tag not code tag
 }
 
-class DropDown{
-  constructor(element){
+class DropDown {
+  constructor(element) {
     this.element = element;
+    //creates an empty div element
     this.listDiv = document.createElement('div');
   }
 
-  init(){
+  init() {
+    //grab the content of the dropdown
     const content = document.querySelector(`#${this.element.id} .dropdown-content`);
+
+    //grab all the childen/items/options of the dropdown
     const children = document.querySelectorAll(`#${this.element.id} .dropdown-item`);
-    children.forEach((child) =>{
+    children.forEach((child) => {
       const originalOnClick = child.onclick;
-      child.addEventListener('click', () =>{
+      child.addEventListener('click', () => {
+        //fires the oroginal on click attribute
         originalOnClick();
+
+        //sets the button text
         this.element.childNodes[0].nodeValue = child.innerText;
-      }) 
+      })
     })
 
+    //remove the content from element
     this.element.removeChild(content);
+
+    //apply css
     this.listDiv.classList.add('dropdown-div');
+
+    //append to respective parents
     this.listDiv.appendChild(content);
     this.element.appendChild(this.listDiv);
 
-    this.element.addEventListener('click', (e) =>{
-      if(e.target != this.element && e.target.tagName != "svg" && e.target.tagName != "path") return;
+
+    this.element.addEventListener('click', (e) => {
+      //this is done to prevent listening to click from the dropdown content/items
+      if (e.target != this.element && e.target.tagName != "svg" && e.target.tagName != "path") return;
+
       this.listDiv.classList.toggle('active');
     })
 
-    this.listDiv.addEventListener('click', () =>{
-      this.listDiv.classList.remove('active');
+    this.listDiv.addEventListener('click', () => {
+      this.hide();
     })
   }
 
-  hide(){
+  hide() {
     this.listDiv.classList.remove('active');
   }
 }
